775132dc600d3f1e57204ec950736147524c4e4c
Alireza Izadi <alirezaizad@outlook.com>
2023-03-30 11:53:39 +0430

    Added generate random  password functionality to the application

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    password_entry.delete(0, END)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    nr_letters= random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+    random_letters = [random.choice(letters) for _ in range(1, nr_letters + 1)]
+    random_symbols = [random.choice(symbols) for _ in range(1, nr_symbols + 1)]
+    random_numbers =[random.choice(numbers) for _ in range(1, nr_numbers + 1)]
+    password = random_letters + random_symbols + random_numbers
+    random.shuffle(password)
+    password_entry.insert(END, "".join(password))
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -43,7 +58,7 @@ password_label.grid(column=0, row=3)
 password_entry = Entry(width=30)
 password_entry.grid(column=1,row=3, sticky="w")
 #Password Generator Button
-password_generate_btn = Button(text="Generate Password")
+password_generate_btn = Button(text="Generate Password", command=generate_password)
 password_generate_btn.grid(column=2, row=3)
 #Add Button
 add_button = Button(text="Add", width=36, command=save)
